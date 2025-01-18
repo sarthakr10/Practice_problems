1c1a2fa973507e7162b95072569bfe90d238b970
sarthakr10 <rastogisarthak35@gmail.com>
2025-01-18 15:27:55 +0530

    Prime number

---
+++ prime.java
@@ -0,0 +1,21 @@
+public class prime {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
